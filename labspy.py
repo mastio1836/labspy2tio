c0c91732d62e24f2f8bd65ba350fa55f77c9ce75
mastio1836 <56244106+mastio1836@users.noreply.github.com>
2019-11-01 11:34:27 +0700

    Add files via upload

---
+++ labspy.py
@@ -0,0 +1,16 @@
+Bilangan1 = int(input("Masukan Bilangan 1:"))
+Bilangan2 = int(input("Masukan Bilangan 2:"))
+Bilangan3 = int(input("Masukan Bilangan 3:"))
+
+if int(Bilangan1) and (Bilangan1 > Bilangan3):
+    print("Nilai terbesarnya adalah :", Bilangan1)
+    Terbesar = Bilangan1
+    Nobil = "Bilangan 1"
+elif (Bilangan2 > Bilangan3) and (Bilangan2 > Bilangan1, Bilangan2):
+    Terbesar = Bilangan2
+    Nobil = "Bilangan 2"
+else:
+    Terbesar = Bilangan3
+    Nobil = "Bilangan 3"
+
+    print("Bilangan yang terbesar adalah",Nobil,"dengan nilai", Terbesar)
